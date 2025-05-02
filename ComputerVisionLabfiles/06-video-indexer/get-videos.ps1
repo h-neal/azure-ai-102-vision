bb63aae691060e0f1c694dea772cfcb2a2b7020a
h-neal <hnneal2@hotmail.co.uk>
2025-05-02 14:54:44 +0100

    Remove ComputerVisionLabfiles after merging into Labfiles

--- ComputerVisionLabfiles/06-video-indexer/get-videos.ps1
+++
@@ -1,9 +0,0 @@
-$account_id="06da8c9d-ea72-4b82-a3e7-2a8f11b6c7f6"
-$api_key="9976781c49714547aaf56c3c377a10a3"
-$location="trial"
-
-# Call the AccessToken method with the API key in the header to get an access token
-$token = Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/auth/$location/Accounts/$account_id/AccessToken" -Headers @{'Ocp-Apim-Subscription-Key' = $api_key}
-
-# Use the access token to make an authenticated call to the Videos method to get a list of videos in the account
-Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/$location/Accounts/$account_id/Videos?accessToken=$token" | ConvertTo-Json -Depth 6
